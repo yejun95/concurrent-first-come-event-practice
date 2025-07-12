a91c20670519969c860223c67ccfb137cd41af22
jun <xodlf100@gmail.com>
2025-07-12 23:55:43 +0900

    feat redis incr을 이용한 쿠폰 count 보장

--- api/src/main/java/com/example/api/service/ApplyService.java
+++ api/src/main/java/com/example/api/service/ApplyService.java
@@ -1,6 +1,7 @@
 package com.example.api.service;
 
 import com.example.api.domain.Coupon;
+import com.example.api.repository.CouponCountRepository;
 import com.example.api.repository.CouponRepository;
 import org.springframework.stereotype.Service;
 
@@ -9,12 +10,24 @@ public class ApplyService {
 
     private final CouponRepository couponRepository;
 
-    public ApplyService(CouponRepository couponRepository) {
+    private final CouponCountRepository couponCountRepository;
+
+    public ApplyService(CouponRepository couponRepository, CouponCountRepository couponCountRepository) {
         this.couponRepository = couponRepository;
+        this.couponCountRepository = couponCountRepository;
     }
 
     public void apply(Long userId) {
-        long count = couponRepository.count();
+        // redis incr key:value
+        // 127.0.0.1:6379> incr coupon_count
+        // (integer) 1
+        // 127.0.0.1:6379> incr coupon_count
+        // (integer) 2
+        // 실제 db에 저장되어 있는 값이 아니라, redis incr을 활용하여 count를 측정하는 것
+        // redis는 싱글스레드 기반으로 동작하므로 데이터에 대한 정합성 유지 가능
+        Long count = couponCountRepository.increment();
+
+        //long count = couponRepository.count();
 
         if (count > 100) {
             return;
